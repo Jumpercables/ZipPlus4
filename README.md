a44ed09941399bcc532df1d9725ce96e057883ec
Jumpercables <kyle.baesler@gmail.com>
2015-09-09 07:34:44 -0700

    Added README and license

---
+++ README.md
@@ -0,0 +1,7 @@
+# Zip+4 #
+An address parsing library that follows the US Postal Service standards.
+- Still in development
+
+### Requirements ###
+- 4.5 .NET Framework
+- Visual Studio 2013
--- tests/Zip+4.Tests/StandardAddressTest.cs
+++ tests/Zip+4.Tests/StandardAddressTest.cs
@@ -3,19 +3,43 @@
 namespace ZipPlus4.Tests
 {
     [TestClass]
-    public class DeliveryAddressTest
+    public class StandardAddressTest
     {
         // "NEWTON AVE  BETWEEN CEASER CHAVEZ AND DEWEY"
         // "1529 ROSE ST. N/O CHERRY PL."
         // "276 N. EL CAMINO REAL # 51"
         // "4125 PACIFIC HWY"
         // "REC ROOM CAMINITO CORRIENTE"
-        // 
+        // 4326 AND  4316 LONGBRANCH AVENUE
 
         #region Public Methods
-        
+
         [TestMethod]
-        public void DeliveryAddress_DirectonalAsPartOfStreet()
+        public void StandardAddress_BusinessAddress()
+        {
+            var value = PostalAddress.Parse<StandardAddress>("REC ROOM CAMINITO CORRIENTE");
+            Assert.AreEqual(null, value.Number);
+            Assert.AreEqual(null, value.Fraction);
+            Assert.AreEqual(null, value.PreDirectional);
+            Assert.AreEqual("REC ROOM CAMINITO CORRIENTE", value.Street);
+            Assert.AreEqual(null, value.PostDirectional);
+            Assert.AreEqual(null, value.Suffix);
+        }
+
+        [TestMethod]
+        public void StandardAddress_CrossStreetAddress()
+        {
+            var value = PostalAddress.Parse<StandardAddress>("4326 AND 4316  LONGBRANCH AVENUE");
+            Assert.AreEqual("4316", value.Number);
+            Assert.AreEqual(null, value.Fraction);
+            Assert.AreEqual(null, value.PreDirectional);
+            Assert.AreEqual("LONGBRANCH", value.Street);
+            Assert.AreEqual(null, value.PostDirectional);
+            Assert.AreEqual("AVE", value.Suffix);
+        }
+
+        [TestMethod]
+        public void StandardAddress_DirectonalAsPartOfStreet()
         {
             var value = PostalAddress.Parse<StandardAddress>("BAY WEST DR");
             Assert.AreEqual(null, value.Number);
@@ -27,7 +51,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_HighwayEndsWithNumber()
+        public void StandardAddress_HighwayEndsWithNumber()
         {
             var value = PostalAddress.Parse<StandardAddress>("15417 OLD HWY 80");
             Assert.AreEqual("15417", value.Number);
@@ -39,7 +63,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_HighwayWithPerDirectionalEndsWithNumber()
+        public void StandardAddress_HighwayWithPerDirectionalEndsWithNumber()
         {
             var value = PostalAddress.Parse<StandardAddress>("1010 S.COAST HWY 101");
             Assert.AreEqual("1010", value.Number);
@@ -51,7 +75,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_HighwayWithPerDirectionalWithoutSuffixEndsWithNumber()
+        public void StandardAddress_HighwayWithPerDirectionalWithoutSuffixEndsWithNumber()
         {
             var value = PostalAddress.Parse<StandardAddress>("1604 N.COAST 101");
             Assert.AreEqual("1604", value.Number);
@@ -63,7 +87,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_MalformedFraction()
+        public void StandardAddress_MalformedFraction()
         {
             var value = PostalAddress.Parse<StandardAddress>("3456 3/ E 7TH ST");
             Assert.AreEqual("3456", value.Number);
@@ -75,7 +99,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_NumberAndFractionAndStreetAndSuffixAndPostDirectional()
+        public void StandardAddress_NumberAndFractionAndStreetAndSuffixAndPostDirectional()
         {
             var value = PostalAddress.Parse<StandardAddress>("5613 1/2 HUNTINGTON DR N");
             Assert.AreEqual("5613", value.Number);
@@ -87,7 +111,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_NumberAndPreDirectionalAndStreetAndSuffixAndPostDirectional()
+        public void StandardAddress_NumberAndPreDirectionalAndStreetAndSuffixAndPostDirectional()
         {
             var value = PostalAddress.Parse<StandardAddress>("1031 W GARVEY AVE N");
             Assert.AreEqual("1031", value.Number);
@@ -100,7 +124,7 @@ namespace ZipPlus4.Tests
 
 
         [TestMethod]
-        public void DeliveryAddress_NumberAndStreetAndSuffix()
+        public void StandardAddress_NumberAndStreetAndSuffix()
         {
             var value = PostalAddress.Parse<StandardAddress>("49 Alvarado RD");
             Assert.AreEqual("49", value.Number);
@@ -113,7 +137,7 @@ namespace ZipPlus4.Tests
 
 
         [TestMethod]
-        public void DeliveryAddress_NumberAndStreetAndSuffixPeriod()
+        public void StandardAddress_NumberAndStreetAndSuffixPeriod()
         {
             var value = PostalAddress.Parse<StandardAddress>("5148 63rd St.");
             Assert.AreEqual("5148", value.Number);
@@ -125,7 +149,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_NumberAndTwoStreetsAndSuffix()
+        public void StandardAddress_NumberAndTwoStreetsAndSuffix()
         {
             var value = PostalAddress.Parse<StandardAddress>("2002 JIMMY DURANTE BOULEVARD");
             Assert.AreEqual("2002", value.Number);
@@ -137,7 +161,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_NumberRange()
+        public void StandardAddress_NumberRange()
         {
             var value = PostalAddress.Parse<StandardAddress>("1173-1175 FLORIDA STREET");
             Assert.AreEqual("1173", value.Number);
@@ -149,7 +173,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_SingleLetterStreet()
+        public void StandardAddress_SingleLetterStreet()
         {
             var value = PostalAddress.Parse<StandardAddress>("2776 B ST");
             Assert.AreEqual("2776", value.Number);
@@ -161,7 +185,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_SuffixAsPartOfStreet()
+        public void StandardAddress_SuffixAsPartOfStreet()
         {
             var value = PostalAddress.Parse<StandardAddress>("213 ALLEY S AVE");
             Assert.AreEqual("213", value.Number);
@@ -173,7 +197,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_TwoSuffixesWithNumber()
+        public void StandardAddress_TwoSuffixesWithNumber()
         {
             var value = PostalAddress.Parse<StandardAddress>("1370 OAK DR FOOTHILL DR");
             Assert.AreEqual("1370", value.Number);
@@ -185,7 +209,7 @@ namespace ZipPlus4.Tests
         }
 
         [TestMethod]
-        public void DeliveryAddress_TwoSuffixesWithoutNumber()
+        public void StandardAddress_TwoSuffixesWithoutNumber()
         {
             var value = PostalAddress.Parse<StandardAddress>("ROSE FLOWER DR. S/O SUNFLPWER ST.");
             Assert.AreEqual(null, value.Number);
